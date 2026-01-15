6367fc20ca5d18074b98547259cee328ada7fa39
tonovarela <tonovarela@live.com>
2026-01-15 08:54:23 -0600

    Actualizar README y mejorar manejo de errores en endpoints de posts

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from fastapi import FastAPI, Query,Body
+from fastapi import FastAPI, Query,Body,HTTPException
 
 app = FastAPI(title="Mini blog")
-
-
 BLOG_POST =[
     {"id":1,"title":"Hola desde FastAPI","content":"Mi primer post con FastAPI"},
     {"id":2,"title":"Segundo post","content":"Mi primer post con FastAPI1"},
     {"id":3,"title":"Tercer post","content":"Mi primer post con FastAPI2"},
     {"id":4,"title":"Cuarto post","content":"Mi primer post con FastAPI3"},
 ]
-
 
 @app.get("/")
 def home():
@@ -31,17 +28,16 @@ def get_post(post_id: int,include_content:bool= Query(default=False,description=
             id_, title = post_found["id"], post_found["title"]
             return {"data":{ "id":id_,"title":title}}            
         return {"data":post_found}                     
-    return {"message":"Post not found"}
-
+    raise HTTPException(status_code=404, detail="Post no encontrado")
 
 
 @app.post("/posts")
 def create_post(post:dict =Body(...)):
     print(post)
     if "title" not in post or "content" not in post:        
-        return {"error":"Title y Content son requeridos"}
+        raise HTTPException(status_code=400, detail="Title y Content son requeridos")
     if not str(post["title"]).strip():
-        return {"error":"Title no puede estar vacio"}    
+        raise HTTPException(status_code=400, detail="Title no puede estar vacio")  
     new_id = (BLOG_POST[-1]["id"])+1 if BLOG_POST else 1
     
     new_post= {"id":new_id,
@@ -54,5 +50,29 @@ def create_post(post:dict =Body(...)):
         "data":new_post
         
     }
+  
+  
+@app.put("/posts/{post_id}")
+def update_post(post_id:int, post:dict=Body(...)):
+    post_found = next((post for post in BLOG_POST if post['id'] == post_id), None)        
+    if not post_found:
+        raise HTTPException(status_code=404, detail="Post no encontrado")    
+    if "title" in post:
+        if not str(post["title"]).strip():
+            raise HTTPException(status_code=400, detail="Title no puede estar vacio")  
+        post_found["title"]=post["title"]
+    if "content" in post:
+        post_found["content"]=post["content"]
+    return {
+        "message":"Post actualizado",
+        "data":post_found        
+    }  
     
     
+@app.delete("/posts/{post_id}")
+def delete_post(post_id:int):
+    for index,post in enumerate(BLOG_POST):
+        if post["id"] == post_id:
+            BLOG_POST.pop(index)
+            return  {"message":"Post borrado"}
+    raise HTTPException(status_code=404,detail="Post no encontrado")    
